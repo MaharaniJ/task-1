c76156955d4b706c1be49f641299794ae1d5af51
MaharaniJ <maharani.jeyaraman@outlook.com>
2023-05-02 10:29:42 +0530

    node-1

---
+++ index.js
@@ -0,0 +1,44 @@
+// const {timestamp} = require(console);
+// const { timeStamp } = require("console");
+// const express = require("express")
+// const fs = require("fs");
+// const { get } = require("http");
+// const app = express()
+
+// const date = new Date()
+// const dotenv = require("dotenv").config
+
+// app.get("/createtimestam", function(req, res){
+//     fs.writeFile(`./${date.getDate()}-${date.getMonth()+1}-${date.getFullYear()}-${date.getHours()}-${date.getMinutes()}.txt`,
+//     `Date:${date.getDate()}-${date.getMonth()+1}-${date.getFullYear()}
+//     timeStamp:${new Date()}`,
+//     (err)=>{
+//         if(err) throw err;
+//         console.log("File Created")
+//     });
+//     res.json("File Created")
+// })
+// app.listen(process.env.PORT || 3000)
+
+
+
+
+const express = require('express') 
+const fs = require('fs') 
+const app = express() 
+ 
+const date = new Date(); 
+const dotenv = require('dotenv').config() 
+ 
+app.get("/createtimestamp", function (req,res){ 
+    fs.writeFile(
+        `./${date.getDate()}-${date.getMonth()+1}-${date.getFullYear()}-${date.getHours()}-${date.getMinutes()}.txt`, 
+    `Date : ${date.getDate()}-${date.getMonth()+1}-${date.getFullYear()} timeStamp : ${new Date()}`, 
+    (err) => { 
+        if (err) throw err; 
+        console.log("File Created") 
+    }); 
+    res.json ("File Created") 
+}); 
+ 
+app.listen(process.env.PORT || 3000)
